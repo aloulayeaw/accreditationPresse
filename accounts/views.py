822730022e9d2e1345fe6b99aa24a14e9ae805d8
aloulayeaw <47934153+aloulayeaw@users.noreply.github.com>
2025-01-11 23:55:35 +0100

    Deploy new Version App Web

--- accounts/views.py
+++ accounts/views.py
@@ -14,7 +14,7 @@ import random
 from django.utils.crypto import get_random_string
 from django.core.mail import send_mail
 #from dashboard.twitter_utils import get_twitter_api
-#import tweepy
+import tweepy
 
 # Create your views here.
 
@@ -29,7 +29,7 @@ def login(request):
         
         if user is not None:
             auth.login(request, user)
-            messages.success(request, 'You are now logged in.')
+            #messages.success(request, 'You are now logged in.')
             return redirect('dashboard/stats')
         else:
             messages.error(request, 'Invalid login credentials')
